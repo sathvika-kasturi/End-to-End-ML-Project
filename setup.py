c3fdc2dd3c6d95ef5676c60475cf42e8658f7fd8
kasturi sathvika <kasturisathwika@gmail.com>
2026-06-17 19:17:39 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup,find_packages
+
+from typing import List
+HYPHEN_E_DOT='-e.'
+
+def get_requirements(file_path:str)->list[str]:
+    '''this function will return the list of requirements'''
+    
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+    requirements=[req.replace('/n','') for req in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='end to end ml project',
+    version='0.0.1',
+    author='kasturi sathvika',
+    author_email='kasturisathvika@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
+
